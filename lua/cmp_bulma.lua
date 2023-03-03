23da57ab617ec6829223c0521d0fba1d35f1a388
Gary Hurtz <garyhurtz@gmail.com>
2023-03-02 17:49:05 -0800

    initial commit

---
+++ lua/cmp_bulma.lua
@@ -0,0 +1,57 @@
+local config = require("cmp.config")
+
+local source_name = "bulma"
+
+local default_config = {
+	label = "[" .. (source_name:gsub("^%l", string.upper)) .. "]",
+	keyword_pattern = [[\a\+-\=]],
+	filetypes = {
+		-- html and templating
+		"html",
+		"jinja.html",
+		-- javascript
+		"javascript",
+		"typescript",
+		-- css and related
+		"css",
+		"less",
+		"sass",
+		"scss",
+	},
+}
+
+local Source = {
+	config = default_config,
+}
+
+function Source:new()
+	local user_config = config.get_source_config(source_name) or {}
+
+	if user_config.option ~= nil and user_config.option.filetypes ~= nil then
+		vim.list_extend(self.config.filetypes, user_config.option.filetypes)
+	end
+
+	return self
+end
+
+function Source.get_debug_name()
+	return source_name
+end
+
+function Source:is_available()
+	return vim.tbl_contains(self.config.filetypes, vim.bo.filetype)
+end
+
+function Source:get_keyword_pattern()
+	return self.config.keyword_pattern
+end
+
+function Source:complete(_, callback)
+	callback(require("entries"))
+end
+
+function Source:setup()
+	require("cmp").register_source(source_name, Source)
+end
+
+return Source:new()
---
+++ readme.md
@@ -0,0 +1,61 @@
+# Bulma completion source for nvim-cmp
+
+Bulma class-name completion source for [nvim-cmp](https://github.com/hrsh7th/nvim-cmp).
+
+Entries updated per Bulma v0.9.4.
+
+## Installation
+
+Using [Packer](https://github.com/wbthomason/packer.nvim):
+
+```lua
+use({
+  'garyhurtz/cmp-bulma.nvim',
+  config = {
+    require('cmp_bulma').setup()
+  }
+})
+```
+
+## Setup
+
+Add this source to your `nvim-cmp` configuration:
+
+```lua
+cmp.setup({
+  sources = cmp.config.sources({
+        -- other sources...
+        { name = "bulma" }
+    )
+})
+```
+
+## Configuration
+
+By default, this source is available for the following file types:
+
+- html
+
+- css, sass, scss, less
+
+- javascript, typescript
+
+Additional file types can be added using _option.filetypes_:
+
+```lua
+cmp.setup({
+  sources = cmp.config.sources({
+    -- other sources...
+    {
+        name = "bulma"
+        option = {
+            filetypes = {
+                "jinja.html",
+                "markdown",
+                ...
+            },
+        }
+    },
+  })
+})
+```
